d82e08279157eb0391302bce89763f16adf848f3
Mayur Gavhane <2020bec075@sggs.ac.in>
2024-08-02 02:42:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,5 +36,5 @@ app.use('/api/movies', movieRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(Server running on port ${PORT});
 });
